3734033c8bd949efc57eee36656a89f273edded7
BuildTools <unconfigured@null.spigotmc.org>
2025-11-28 21:12:56 +0200

    feat: refactor server setup; implement socket.io for real-time messaging; update email functionality with nodemailer

--- server.cjs
+++ server.cjs
@@ -1,12 +1,11 @@
+require('dotenv').config();
 const os = require("os");
 const math = require("mathjs");
-const fs = require("fs");
-const EventEmitter = require("events");
-const http = require("http");
+const cors = require("cors");
 const express = require("express");
-const Joi = require("joi");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const nodemailer = require("nodemailer");
 
 const foods = [
     { id: 1, meals: ["Pizza", "Salad", "Soup"] },
@@ -15,17 +14,33 @@ const foods = [
 ];
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {cors: {origin: "*"}});
 
 app.use(express.json());
 
 app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:5173', 'https://justus-lipiainen.github.io', 'https://justus-lipiainen.github.io/test', 'http://localhost:5173/test']
-}))
+}));
 
 
-io.on("connection", (socket) => {
-    console.log(`Connection with id: ${socket.id}`)
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    }
 });
+
+const mailOptions = {
+    from: 'lipiainen.noreply@gmail.com',
+    to: 'justus.lipiainen@gmail.com',
+    subject: 'HTML Email',
+    html: '<h1>Hello!</h1><p>This is an HTML email.</p>'
+};
+
+
+transporter.sendMail(mailOptions);
 
 app.get("/", (req, res) => {
     const response = [
@@ -53,12 +68,19 @@ app.post("/api/foods", (req, res) => {
     res.send(food);
 });
 
-app.put("/api/foods/:id", (req, res) => {
-    const food = foods.find(c => c.id === parseInt(req.params.id));
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+    io.emit("welcome", { message: "Welcome to the Socket.IO server!" });
 
-    food.meals = req.body.meals;
-    res.send(food);
+    socket.on("message", (msg) => {
+        console.log("Received message from client:", msg.data);
+        socket.emit("msgToClient", { data: `Message from (${socket.id}) ${msg.data}` });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {console.log(`Server started. Port: ${port}`)});
+const port2 = process.env.PORT || 6767;
+httpServer.listen(port2, () => {console.log(`Server started. Port: ${port2}`)});
